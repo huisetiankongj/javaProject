5d4a43fd905493f262b97aebf0b859aa4b5231c5
Administrator <Administrator@WIN-4EE1VO51HSF>
2016-06-16 01:00:53 +0800

    注释中文乱码修改

--- src/main/java/com/czx/keys/_static/demo001/Test.java
+++ src/main/java/com/czx/keys/_static/demo001/Test.java
@@ -2,14 +2,14 @@ package com.czx.keys._static.demo001;
 
 /**
  * 执行过程:
- * 	首先加载Test类，执行Test类中的static块�?
- * 	接着执行new MyClass(),而MyClass类还没被加载，则�?��加载MyClass类�?
- * 	在加载MyClass类的时�?，发现MyClass类继承Test类，但是由于Test类已经被加载了，
- * 	�?��只需要加载MyClass类，那么就会执行MyClass类的static块�?在加载完后，
- * 	就�?过构造器来生成对象，而在生成对象的过程中，必须先初始化类的成员变量，因此会执行Test
- * 	中的Person person = new Person(),而Person类还没有被加载过，因此会先加�?
- * 	Person类并执行static块�?并执行父类的构�?器，完成父类的初始化，然后就可以初始化自身了�?
- * 	因此会接�?��行MyClass中的Person person = new Person()，最后执行MyClass的构造器
+ * 	首先加载Test类，执行Test类中的static块。
+ * 	接着执行new MyClass(),而MyClass类还没被加载，则需要加载MyClass类。
+ * 	在加载MyClass类的时候，发现MyClass类继承Test类，但是由于Test类已经被加载了，
+ * 	所以只需要加载MyClass类，那么就会执行MyClass类的static块。在加载完后，
+ * 	就通过构造器来生成对象，而在生成对象的过程中，必须先初始化类的成员变量，因此会执行Test
+ * 	中的Person person = new Person(),而Person类还没有被加载过，因此会先加载
+ * 	Person类并执行static块。并执行父类的构造器，完成父类的初始化，然后就可以初始化自身了。
+ * 	因此会接着执行MyClass中的Person person = new Person()，最后执行MyClass的构造器
  * @author Administrator
  *
  */
